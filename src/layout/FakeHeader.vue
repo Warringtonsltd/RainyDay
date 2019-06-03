a01c0e90b4c554062e413eace08a22240aef0df5
Faheem <f4heem@gmail.com>
2019-06-03 19:25:20 +0200

    Update latest

---
+++ src/layout/FakeHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="header-global">
+        <base-nav class="navbar-main nav-custom" transparent type="" effect="light">
+            <a slot="brand" class="navbar-brand mr-lg-5 text-primary" href="/">
+                Cancel
+            </a>
+            <div slot="brand" class="navbar-brand-custom text-primary" href="/">
+                 <input
+                    placeholder="Search"
+                    class="form-control custom-control"
+                    aria-describedby="addon-right addon-left">
+            </div>
+            
+        </base-nav>
+    </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+
+export default {
+  components: {
+    BaseNav,
+    BaseDropdown
+  }
+};
+</script>
+<style>
+</style>
